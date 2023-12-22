0579225ca9e2e7a52de2b3b5b2eb73385e82107d
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2023-12-22 22:21:11 +0900

    Update 요세푸스 문제.py

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,16 +1,22 @@
 from collections import deque
 
-
+# Input: n, k
 n, k = map(int, input().split())
 
 
+# Initialize the input queue with numbers from 1 to n
 input_queue = deque(range(1, n+1))
+
+# Initialize an output queue to store the order of removed elements
 output_queue = deque()
 
 
 
 while len(input_queue) > 1 :
+    # Rotate the input queue to make the (k-1)th element the first one
     input_queue.rotate(-(k - 1))
+
+    # Remove and append the first element (k-1th element after rotation) to the output queue
     i = input_queue.popleft()
     output_queue.append(i)
 
